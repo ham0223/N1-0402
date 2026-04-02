0dd1b446487c6833137c09727b4984b8bae100d4
ham0223 <126130711+ham0223@users.noreply.github.com>
2026-04-02 15:49:23 +0800

    Update diy-part2.sh

--- N1/Lede/diy-part2.sh
+++ N1/Lede/diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+set -e  # 任何命令失败立即退出，防止静默跳过错误
 
 # 修改IP   第一行19.07的路径   第二行23.05的路径
 #sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
@@ -9,31 +9,39 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/luci2/bin/config_gener
 #sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
 
-# 拉取源码
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
-rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-
-# passwall官方方法：移除 openwrt feeds 自带的核心库
+# 彻底清理 feeds 自带的冲突项
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-# 移除 openwrt feeds 过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-mosdns feeds/packages/net/mosdns
+rm -rf feeds/packages/net/openlist
+rm -rf feeds/luci/applications/luci-app-openlist
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/luci/applications/luci-app-nikki
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openlist2
+
+rm -rf feeds/luci/luci-app-mjpg-streamer
+rm -rf feeds/packages/onionshare-cli
+rm -rf package/feeds/luci/luci-app-mjpg-streamer
+rm -rf package/feeds/packages/onionshare-cli
+sed -i '/mjpg-streamer/d' .config 2>/dev/null || true
+sed -i '/onionshare/d' .config 2>/dev/null || true
 
 
-# 备份科学插件源：移除 openwrt feeds 自带的核心包
-#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
-# 更新 golang 1.25 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+# 克隆 Passwall 2
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
+rm -rf package/passwall-packages/shadowsocksr-libev
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/passwall2
 
+
+# 其他插件
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
+git clone https://github.com/gdy666/luci-app-lucky.git --depth=1 package/lucky
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 --depth=1 package/mosdns
+git clone https://github.com/sbwml/luci-app-openlist2 --depth=1 package/openlist2
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki --depth=1 package/nikki
+git clone https://github.com/vernesong/OpenClash --depth=1 package/openclash
 
 # 临时修复acpid,aliyundrive-webdav,xfsprogs,perl-html-parser,v2dat 导致的编译失败问题
 #sed -i 's#flto#flto -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/acpid/Makefile
