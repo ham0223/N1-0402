182868e221e6acb322095e0e3ab5642f49691859
ham0223 <126130711+ham0223@users.noreply.github.com>
2026-04-04 12:50:44 +0800

    Update diy-part22.sh

--- N1/Lede/diy-part22.sh
+++ N1/Lede/diy-part22.sh
@@ -7,7 +7,7 @@ sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 2. 彻底清理 feeds 冲突 (防止 PassWall, Nikki, TurboACC 等重复报错)
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls,zerotier,socat}
-rm -rf feeds/luci/applications/luci-app-{passwall*,mosdns,lucky,nikki,openclash,zerotier,socat,turboacc,dockerman,samba4,vsftpd,softethervpn}
+rm -rf feeds/luci/applications/luci-app-{passwall*,mosdns,lucky,nikki,openclash}
 
 # 3. 插件仓库拉取 (含官方 PassWall & 额外增强插件)
 # PassWall 官方最新版
@@ -21,11 +21,7 @@ git clone https://github.com/vernesong/OpenClash --depth=1 package/openclash
 git clone https://github.com/gdy666/luci-app-lucky.git --depth=1 package/lucky
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 --depth=1 package/mosdns
 git clone https://github.com/sbwml/luci-app-openlist2 --depth=1 package/openlist2
-
-# 系统与加速插件
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
-# 注：Docker, Samba4, ZeroTier, Socat, Turboacc 等通常在常用 feeds 中已包含，直接在 config 开启即可
-
 
 # 4. 修复系统库依赖 (防止 armsr 架构下的编译中断)
 sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/Makefile
